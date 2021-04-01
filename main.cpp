d0198b17105883d0bf5136bfed4ee53b35bf4f77
Dinh Khoi <dinhkhoi12tin@gmail.com>
2021-04-01 15:42:23 +0700

    cl

--- main.cpp
+++ main.cpp
@@ -5,9 +5,6 @@ int main(){
     cout<<"hen nhau 8 gio toi va chu nhat nua !! ";
     cout<<"push lan 2 ne!!";
     cout<<"con cat!!";
-<<<<<<< HEAD
     cout<<"cai lon";
-=======
     cout<<"ma";
->>>>>>> 87c169df5385bfb6eebf0b0c2d36ccd424259d79
 }
