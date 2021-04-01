df5841f59b100847d2d42cf05d9cf0a620f8fa97
Dinh Khoi <dinhkhoi12tin@gmail.com>
2021-04-01 15:57:30 +0700

    lan cuoi

--- main.cpp
+++ main.cpp
@@ -2,11 +2,6 @@
 using namespace std;
 int main(){
 
-    cout<<"hen nhau 8 gio toi va chu nhat nua !! ";
-    cout<<"push lan 2 ne!!";
-    cout<<"con cat!!";
-    cout<<"cai lon";
-    cout<<"ma12323";
-    cout<<"conflict ne!!";
-    cout<<"up thu duoc ko chac ko duoc,buc vai lon";
+    cout<<"sua code nay lai !!";
+    
 }
