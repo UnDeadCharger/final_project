f9879a3fdb8944dc01e7997d2af87bb0d0139e3e
UnDeadCharger <rexfury121@gmail.com>
2021-04-02 21:21:19 +0700

    added displayFrame Function
    first step to create menu

--- main.cpp
+++ main.cpp
@@ -1,14 +1,48 @@
 #include<iostream>
+#include<stdlib.h>
+#include<windows.h>
 using namespace std;
-int main(){
-
-    cout<<"sua code nay lai !!";
-    cout<<"moi vao code,confilct ne";
+void createframe();
+void gotoxy(int x, int y)
+{
+    static HANDLE h = NULL;
+    if (!h)
+        h = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD c = { x, y };
+    SetConsoleCursorPosition(h, c);
+}
+int main() {
+    createframe();
+    return 0;
 }
 void displayUI(int options)
 {
     do
     {
-        
-    } while (/* condition */);
+        system("CLS");
+        createframe();
+
+        cin >> options;
+    } while (options != 0);
 }
+void createframe()
+    {
+
+    int x = 60;
+    int y = 10;
+    for (int i = 1; i < x; i++) // tao dong ngang
+    {
+        gotoxy(i, 0); printf("%c", 205); // dong ngang tren
+        gotoxy(i, y); printf("%c", 205);  // dong ngang duoi
+    }
+    for (int i = 1; i < y; i++)  // dong ke thang dung
+    {
+        gotoxy(0, i); printf("%c", 186); // dong thang ben trai
+        gotoxy(x, i); printf("%c", 186); // dong thang ben phai
+    }
+    gotoxy(x, 0); printf("%c", 187); // cai moc cau ben phai ben tre^n
+    gotoxy(x, y); printf("%c", 188); // cai moc cau ben phai ben duoi
+    gotoxy(0, 0); printf("%c", 201); // cai moc cau ben trai tren
+    gotoxy(0, y); printf("%c", 200); // moc cau ben trai duoi
+    cout << endl;
+ }
